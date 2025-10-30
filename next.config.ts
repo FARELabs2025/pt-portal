bc64dfd27b84bd3d9eaf783c2c4b115918f6ed0d
Abhinavsinghasr <abhinavsingh2530@gmail.com>
2025-10-30 10:17:07 +0530

    Initial commit from Create Next App
    # Conflicts:
    #	.gitignore
    #	README.md
    #	next.config.ts
    #	package-lock.json
    #	package.json
    #	src/app/globals.css
    #	src/app/layout.tsx
    #	src/app/page.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,7 @@
-export default { eslint: { ignoreDuringBuilds: false } }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: false },
+};
+
+export default nextConfig;
