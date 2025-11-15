fb4d1f1471c28dcd6f49a578497acb652ba0bd8b
your-username <roushanraj930468@gmail.com>
2025-11-15 15:44:29 +0530

    Dynamic page Setup Done with Page Creation (Data.ts and ) sand slug(page.tsx)

---
+++ src/app/dashboard/pt-scheme/data.ts
@@ -0,0 +1,104 @@
+
+export const Biological = [
+  { src: "/images/water.png", title: "Water", slug: "water" },
+  { src: "/images/card2.png", title: "Vegetables & Vegetable Products", slug: "vegetables-and-vegetable-products" },
+  { src: "/images/vegan-food.png", title: "Vegan Food", slug: "vegan-food" },
+  { src: "/images/tobacco.png", title: "Tobacco & Tobacco Products", slug: "tobacco-and-tobacco-products" },
+  { src: "/images/tea-leaves.png", title: "Tea", slug: "tea" },
+  { src: "/images/sugar.png", title: "Sugar & Sugar Products", slug: "sugar-and-sugar-products" },
+  { src: "/images/snack.png", title: "Snacks & Instant Mixes", slug: "snacks-and-instant-mixes" },
+  { src: "/images/pollutin.png", title: "Pollution & Environment", slug: "pollution-and-environment" },
+  { src: "/images/oil-seed.png", title: "Oilseeds & By-Products", slug: "oilseeds-and-by-products" },
+  { src: "/images/NS3.png", title: "Nutritional Supplements", slug: "nutritional-supplements" },
+  { src: "/images/nuts.png", title: "Nut & Nut Products", slug: "nut-and-nut-products" },
+  { src: "/images/GM.png", title: "Molecular Analysis", slug: "molecular-analysis" },
+  { src: "/images/milk.png", title: "Milk and Dairy Products", slug: "milk-and-dairy-products" },
+  { src: "/images/meat.png", title: "Meat Authenticity", slug: "meat-authenticity" },
+  { src: "/images/meat1.png", title: "Meat & Meat Products", slug: "meat-and-meat-products" },
+  { src: "/images/marine.png", title: "Marine/Aqua Culture Food Products", slug: "marine-aqua-culture-food-products" },
+  { src: "/images/Jam.png", title: "Jams, Juices, Sauces & Concentrates", slug: "jams-juices-sauces-and-concentrates" },
+  { src: "/images/infant.png", title: "Infant Food", slug: "infant-food" },
+  { src: "/images/honey.png", title: "Honey & Honey Products", slug: "honey-and-honey-products" },
+  { src: "/images/spices.png", title: "Herbs, Spices & Condiments", slug: "herbs-spices-and-condiments" },
+  { src: "/images/Halal.png", title: "Halal Compliance Check", slug: "halal-compliance-check" },
+  { src: "/images/gm1.png", title: "GM Products", slug: "gm-products" },
+  { src: "/images/fruits.png", title: "Fruit & Fruit Products", slug: "fruit-and-fruit-products" },
+  { src: "/images/additives.png", title: "Food Additives & Preservatives", slug: "food-additives-and-preservatives" },
+  { src: "/images/egg.png", title: "Egg & Egg Products", slug: "egg-and-egg-products" },
+  { src: "/images/cosmetic.png", title: "Cosmetics & Essential Oil", slug: "cosmetics-and-essential-oil" },
+  { src: "/images/cocoa.png", title: "Coffee & Cocoa Products", slug: "coffee-and-cocoa-products" },
+  { src: "/images/cereal.png", title: "Cereals Pulses & Cereal Products", slug: "cereals-pulses-and-cereal-products" },
+  { src: "/images/types.png", title: "Cereals and Cereal Products", slug: "cereals-and-cereal-products" },
+  { src: "/images/card3.png", title: "Biopesticides and Biofertilizers", slug: "biopesticides-and-biofertilizers" },
+  { src: "/images/card4.png", title: "Beverages", slug: "beverages" },
+  { src: "/images/card5.png", title: "Bakery and Confectionery Products", slug: "bakery-and-confectionery-products" },
+  { src: "/images/card6.png", title: "AYUSH Products", slug: "ayush-products" },
+  { src: "/images/card7.png", title: "Animal Food and Feed", slug: "animal-food-and-feed" }
+];
+
+export const Chemical = [
+  { src: "/images/ChemicalR1C1.png", title: "Water", slug: "water" },
+  { src: "/images/ChemicalR1C3.png", title: "Tea", slug: "tea" },
+  { src: "/images/ChemicalR1C4.png", title: "Sugar & Sugar Products", slug: "sugar-and-sugar-products" },
+  { src: "/images/ChemicalR2C1.png", title: "Solid Fuels", slug: "solid-fuels" },
+  { src: "/images/ChemicalR2C2.png", title: "Soil and Rock", slug: "soil-and-rock" },
+  { src: "/images/ChemicalR2C3.png", title: "Soap, Detergent & Toiletries", slug: "soap-detergent-and-toiletries" },
+  { src: "/images/ChemicalR2C4.png", title: "Poultry & Poultry Products", slug: "poultry-and-poultry-products" },
+  { src: "/images/ChemicalR3C1.png", title: "Polychlorinated Biphenyls (PCBs)", slug: "polychlorinated-biphenyls-pcbs" },
+  { src: "/images/ChemicalR3C2.png", title: "Pollution and Environment", slug: "pollution-and-environment" },
+  { src: "/images/ChemicalR3C3.png", title: "Plastics and Resins", slug: "plastics-and-resins" },
+  { src: "/images/ChemicalR3C4.png", title: "Petroleum and Products", slug: "petroleum-and-products" },
+  { src: "/images/ChemicalR4C1.png", title: "Pesticide Residues", slug: "pesticide-residues" },
+  { src: "/images/ChemicalR4C2.png", title: "Pesticide Formulations", slug: "pesticide-formulations" },
+  { src: "/images/ChemicalR4C3.png", title: "Paper and Pulp", slug: "paper-and-pulp" },
+  { src: "/images/ChemicalR4C4.png", title: "Paints and Surface Coating", slug: "paints-and-surface-coating" },
+  { src: "/images/ChemicalR5C1.png", title: "Nuts and Nut Products", slug: "nuts-and-nut-products" },
+  { src: "/images/ChemicalR5C2.png", title: "Nutraceuticals & Functional Foods", slug: "nutraceuticals-and-functional-foods" },
+  { src: "/images/ChemicalR5C3.png", title: "Mycotoxins", slug: "mycotoxins" },
+  { src: "/images/ChemicalR5C4.png", title: "Minerals/Trace Metals", slug: "minerals-trace-metals" },
+  { src: "/images/ChemicalR6C1.png", title: "Milk & Dairy Products", slug: "milk-and-dairy-products" },
+  { src: "/images/ChemicalR6C2.png", title: "Marine/Aqua Culture Food Products", slug: "marine-aqua-culture-food-products" },
+  { src: "/images/ChemicalR6C3.png", title: "Lubricants", slug: "lubricants" },
+  { src: "/images/ChemicalR6C4.png", title: "Jams, Juices, Sauces and Concentrates", slug: "jams-juices-sauces-and-concentrates" },
+  { src: "/images/ChemicalR7C1.png", title: "Infant Food", slug: "infant-food" },
+  { src: "/images/ChemicalR7C2.png", title: "Industrial and Fine Chemicals", slug: "industrial-and-fine-chemicals" },
+  { src: "/images/ChemicalR7C3.png", title: "Honey & Honey Products", slug: "honey-and-honey-products" },
+  { src: "/images/ChemicalR7C4.png", title: "Herbs, Spices & Condiments", slug: "herbs-spices-and-condiments" },
+  { src: "/images/ChemicalR8C1.png", title: "Food Additives & Preservatives", slug: "food-additives-and-preservatives" },
+  { src: "/images/ChemicalR8C2.png", title: "Fertilizers", slug: "fertilizers" },
+  { src: "/images/ChemicalR8C3.png", title: "Edible Oils & Fats", slug: "edible-oils-and-fats" },
+  { src: "/images/ChemicalR1C2.png", title: "Vegetables & Vegetable Products", slug: "vegetables-and-vegetable-products" },
+  { src: "/images/ChemicalR8C4.png", title: "Drugs and Pharmaceuticals", slug: "drugs-and-pharmaceuticals" },
+  { src: "/images/ChemicalR9C1.png", title: "Cosmetics and Essential Oil", slug: "cosmetics-and-essential-oil" },
+  { src: "/images/Contaminants.png", title: "Contaminants", slug: "contaminants" },
+  { src: "/images/ChemicalR9C3.png", title: "Coffee & Cocoa Products", slug: "coffee-and-cocoa-products" },
+  { src: "/images/ChemicalR9C4.png", title: "Chlorinated Dioxins & Dibenzofurans", slug: "chlorinated-dioxins-and-dibenzofurans" },
+  { src: "/images/ChemicalR10C1.png", title: "Cereals Pulses & Cereal Products", slug: "cereals-pulses-and-cereal-products" },
+  { src: "/images/ChemicalR10C2.png", title: "Beverages (Alcoholic/Non-Alcoholic)", slug: "beverages-alcoholic-non-alcoholic" },
+  { src: "/images/ChemicalR10C3.png", title: "Bakery & Confectionery Products", slug: "bakery-and-confectionery-products" },
+  { src: "/images/ChemicalR10C4.png", title: "AYUSH Products", slug: "ayush-products" },
+  { src: "/images/ChemicalR11C1.png", title: "Atmospheric Pollution", slug: "atmospheric-pollution" },
+  { src: "/images/ChemicalR11C2.png", title: "Antibiotic Residues", slug: "antibiotic-residues" },
+  { src: "/images/ChemicalR11C3.png", title: "Animal Food and Feed", slug: "animal-food-and-feed" }
+];
+
+
+export const Radiological = [
+  { src: "/images/Radiological1.png", title: "Water", slug: "water" },
+  { src: "/images/Radiological2.png", title: "Sugar & Sugar Products", slug: "sugar-and-sugar-products" },
+  { src: "/images/Radiological3.png", title: "Milk & Dairy Products", slug: "milk-and-dairy-products" },
+  { src: "/images/Radiological4.png", title: "Meat & Meat Products", slug: "meat-and-meat-products" },
+  { src: "/images/Radiological5.png", title: "Cereals Pulses & Cereal Products", slug: "cereals-pulses-and-cereal-products" }
+];
+
+
+export const Mechanical = [
+  { src: "/images/toys.png", title: "Toys", slug: "toys" }
+];
+
+export const categoryData = {
+  biological: { items: Biological, title: "Biological" },
+  chemical: { items: Chemical, title: "Chemical" },
+  radiological: { items: Radiological, title: "Radiological" },
+  mechanical: { items: Mechanical, title: "Mechanical" }
+};
