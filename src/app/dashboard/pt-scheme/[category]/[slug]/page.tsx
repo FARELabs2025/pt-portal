8872d3580ffe5ddd1b4e996aaad78fd0e844d6fa
your-username <roushanraj930468@gmail.com>
2025-11-15 15:44:29 +0530

    Dynamic page Setup Done with Page Creation (Data.ts and ) sand slug(page.tsx)

---
+++ src/app/dashboard/pt-scheme/[category]/[slug]/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { categoryData } from "../../data";
+export default function PTItemPage(){
+        return (
+            <div>
+                hii there From PTITEm Page
+            </div>
+        )
+}
